f0955958e56110b93f7e73426c3b3de6c63a2bba
Mike Maietta <mmaietta@ebay.com>
2014-10-11 15:30:01 -0700

    adding collection view cell

---
+++ Sleigh/DonatedItemCell.m
@@ -0,0 +1,13 @@
+//
+//  DonatedItemCell.m
+//  Sleigh
+//
+//  Created by Mike Maietta on 10/11/14.
+//  Copyright (c) 2014 Wolfpack. All rights reserved.
+//
+
+#import "DonatedItemCell.h"
+
+@implementation DonatedItemCell
+
+@end
