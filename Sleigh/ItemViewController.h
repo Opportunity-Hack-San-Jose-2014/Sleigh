b44d923feec1b2fd33fc91c42d02b659409f79c4
Mike Maietta <mmaietta@ebay.com>
2014-10-11 20:26:37 -0700

    adding View Item

---
+++ Sleigh/ItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  ItemViewController.h
+//  Sleigh
+//
+//  Created by Mike Maietta on 10/11/14.
+//  Copyright (c) 2014 Wolfpack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItemViewController : UIViewController
+
+@end
---
+++ Sleigh/ItemViewController.m
@@ -0,0 +1,21 @@
+//
+//  ItemViewController.m
+//  Sleigh
+//
+//  Created by Mike Maietta on 10/11/14.
+//  Copyright (c) 2014 Wolfpack. All rights reserved.
+//
+
+#import "ItemViewController.h"
+
+@interface ItemViewController ()
+
+@end
+
+@implementation ItemViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
