1c5f6e7351b035ea96e8e143fd50161f9a00d124
Mike Maietta <mmaietta@ebay.com>
2014-12-01 23:09:16 -0800

    cleaning up UI on new donation view controller

--- Sleigh/NewDonationViewController.m
+++ Sleigh/NewDonationViewController.m
@@ -6,18 +6,22 @@
 //  Copyright (c) 2014 Wolfpack. All rights reserved.
 //
 
+#import <AddressBookUI/AddressBookUI.h>
 #import <SDWebImage/UIImageView+WebCache.h>
 #import "NewDonationViewController.h"
 #import "UserDataManager.h"
 #import "DonatedItem.h"
+#import "MBProgressHUD.h"
+#import "UIView+Additions.h"
 
-@interface NewDonationViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate>
+@interface NewDonationViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate, UIActionSheetDelegate>
 
 @property(weak, nonatomic) IBOutlet UIImageView *imageView;
 @property(weak, nonatomic) IBOutlet UITextField *itemCodeTextField;
 @property(weak, nonatomic) IBOutlet UITextField *itemAddressTextField;
 @property(weak, nonatomic) IBOutlet UITextField *itemAvailabilityTextField;
 @property(weak, nonatomic) IBOutlet UITextField *itemPhoneNumberTextField;
+@property(weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
@@ -39,7 +43,7 @@
 {
 	[self.imageView sd_setImageWithURL:[NSURL URLWithString:@"http://ecx.images-amazon.com/images/I/41MCTMPZVML.jpg"]];
 	self.itemCodeTextField.text = @"123456";
-	self.itemAddressTextField.text = @"123 Address Avenue San Jose, CA 95125";
+	self.itemAddressTextField.text = @"1234 Hicks Avenue San Jose, CA 95125";
 	self.itemAvailabilityTextField.text = @"M-F 5-6pm";
 	self.itemPhoneNumberTextField.text = @"408 693 1234";
 }
@@ -55,38 +59,88 @@
 	NSString *address = self.itemAddressTextField.text;
 	NSString *code = self.itemCodeTextField.text;
 	NSString *phoneNumber = self.itemPhoneNumberTextField.text;
+
 	if (schedule.length > 0 && address.length > 0 && code.length > 0 && phoneNumber.length > 0)
 	{
-		DonatedItem *newDonation = [[DonatedItem alloc] initDonatedItemWithDescription:code
-																			   address:address
-																			  schedule:schedule
-																		   phoneNumber:phoneNumber];
+		MBProgressHUD *progressHUD = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+		progressHUD.labelText = @"Validating Address";
 
-		[[UserDataManager sharedInstance] saveDonatedItemToDatabase:newDonation withCompletionBlock:^(NSError *success)
+		CLGeocoder *geocoder = [CLGeocoder new];
+		[geocoder geocodeAddressString:address completionHandler:^(NSArray *placemarks, NSError *error)
 		{
-			if (success == nil)
-				[self.navigationController popViewControllerAnimated:YES];
-			else
-			{
-				UIAlertView *myAlertView = [[UIAlertView alloc] initWithTitle:@"Error"
-																	  message:@"Unable to save item, please try again."
-																	 delegate:nil
-															cancelButtonTitle:@"OK"
-															otherButtonTitles:nil];
+			[MBProgressHUD hideHUDForView:self.view animated:YES];
 
-				[myAlertView show];
+			if (error == nil)
+			{
+				NSMutableArray *addresses = [NSMutableArray new];
+				for (CLPlacemark *placemark in placemarks)
+					[addresses addObject:ABCreateStringWithAddressDictionary(placemark.addressDictionary, NO)];
+
+				if ([placemarks count] > 1)
+				{
+					UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"Please confirm your address"
+																			 delegate:self
+																	cancelButtonTitle:@"Cancel"
+															   destructiveButtonTitle:nil
+																	otherButtonTitles:[addresses componentsJoinedByString:@","], nil];
+				}
+				else
+					[self saveNewItemWithId:code address:[addresses objectAtIndex:0] schedule:schedule phoneNumber:phoneNumber];
 			}
+			else
+				[self displayErrorAlertWithMessage:@"The address you entered is not valid."];
+
 		}];
+
 	}
 	else
-	{
-		UIAlertView *myAlertView = [[UIAlertView alloc] initWithTitle:@"Error"
-															  message:@"Please answer all text fields."
-															 delegate:nil
-													cancelButtonTitle:@"OK"
-													otherButtonTitles:nil];
+		[self displayErrorAlertWithMessage:@"Please answer all text fields."];
+}
 
-		[myAlertView show];
+- (void)saveNewItemWithId:(NSString *)code address:(NSString *)address schedule:(NSString *)schedule phoneNumber:(NSString *)phoneNumber
+{
+	MBProgressHUD *progressHUD = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+	progressHUD.labelText = @"Saving Item";
+
+	DonatedItem *newDonation = [[DonatedItem alloc] initDonatedItemWithDescription:code
+																		   address:address
+																		  schedule:schedule
+																	   phoneNumber:phoneNumber];
+
+	[[UserDataManager sharedInstance] saveDonatedItemToDatabase:newDonation withCompletionBlock:^(NSError *error)
+	{
+		[MBProgressHUD hideHUDForView:self.view animated:YES];
+
+		if (error == nil)
+			[self.navigationController popViewControllerAnimated:YES];
+		else
+			[self displayErrorAlertWithMessage:@"Unable to save item, please try again."];
+	}];
+}
+
+- (void)displayErrorAlertWithMessage:(NSString *)message
+{
+	UIAlertView *myAlertView = [[UIAlertView alloc] initWithTitle:@"Sorry"
+														  message:message
+														 delegate:nil
+												cancelButtonTitle:@"OK"
+												otherButtonTitles:nil];
+
+	[myAlertView show];
+}
+
+#pragma mark - Action Sheet Delegate
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+	if (buttonIndex != actionSheet.cancelButtonIndex)
+	{
+		NSString *schedule = self.itemAvailabilityTextField.text;
+		NSString *code = self.itemCodeTextField.text;
+		NSString *phoneNumber = self.itemPhoneNumberTextField.text;
+		NSString *address = [actionSheet buttonTitleAtIndex:buttonIndex];
+
+		[self saveNewItemWithId:code address:address schedule:schedule phoneNumber:phoneNumber];
 	}
 }
 
@@ -106,12 +160,18 @@
 	NSValue *keyboardFrameBegin = [keyboardInfo valueForKey:UIKeyboardFrameBeginUserInfoKey];
 	CGRect keyboardFrameBeginRect = [keyboardFrameBegin CGRectValue];
 
-	self.view.frame = CGRectMake(0, -keyboardFrameBeginRect.size.height, self.view.frame.size.width, self.view.frame.size.height);
+	self.scrollView.frame = CGRectMake(0, 0, self.view.width, self.view.height - keyboardFrameBeginRect.size.height);
+
+	if (self.scrollView.contentSize.height > self.scrollView.height)
+	{
+		CGPoint offset = CGPointMake(0, self.scrollView.contentSize.height - self.scrollView.height);
+		[self.scrollView setContentOffset:offset animated:YES];
+	}
 }
 
 - (void)keyboardDidHide:(NSNotification *)notification
 {
-	[self.view setFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+	[self.scrollView setFrame:CGRectMake(0, 0, self.view.width, self.view.height)];
 }
 
 #pragma mark - Image Picker Controller delegate methods
@@ -129,7 +189,7 @@
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
 {
 	UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
-//	self.imageView.image = chosenImage;
+	self.imageView.image = chosenImage;
 
 	[picker dismissViewControllerAnimated:YES completion:nil];
 }
